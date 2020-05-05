c202a20df1226b7fd7d75ceb6b05063689b6bace
Corina <corinacvp@gmail.com>
2020-05-05 17:58:58 -0400

    Updates

---
+++ precache-manifest.066a4befa86adb08b85f91cca52c565d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e1b0d38402f4e99a70bed57903ac87",
+    "url": "/people-cards/index.html"
+  },
+  {
+    "revision": "9503e518d8ef96034ff5",
+    "url": "/people-cards/static/css/main.b5d6208b.chunk.css"
+  },
+  {
+    "revision": "c627842f365cee8bb9ee",
+    "url": "/people-cards/static/js/2.ca886677.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/people-cards/static/js/2.ca886677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9503e518d8ef96034ff5",
+    "url": "/people-cards/static/js/main.ac1dcedb.chunk.js"
+  },
+  {
+    "revision": "972b8dafd8f53dc70a60",
+    "url": "/people-cards/static/js/runtime-main.1cc3879f.js"
+  }
+]);
