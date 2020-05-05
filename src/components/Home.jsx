78f9085ffaddc78816d3878d75b0d3ad9a28aba1
Corina <corinacvp@gmail.com>
2020-05-05 16:34:51 -0400

    Funcion que llama a la API

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,48 @@
 import React from 'react'
 import './Home.css'
 
-const Home = () => {
-    return (
-        <div>
-          <h1>People Cards</h1>  
-        </div>
-    )
-}
+export default class Home extends React.Component {
 
-export default Home
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: []  
+        }
+    }
+    
+     getData = () => {
+        fetch('https://randomuser.me/api/?results=20')
+        .then( res => res.json())
+        .then( data => {
+            this.setState({
+                data: data.results
+            })
+
+        }).catch( err => 
+            console.error(err))
+    }
+
+    componentDidMount () {
+        this.getData()
+    }
+
+          
+
+    render(){
+
+        return (
+              <div>
+                  {
+                      this.state.data.map((item) => (
+                          <div>
+                            <p>Nombre: {`${item.name.title} ${item.name.first} ${item.name.last}`}</p>
+                            <p>Telefono: {item.phone}</p>
+                            <p>Email: {item.email}</p>
+                         </div>
+                      ))
+                  }
+              </div>
+         
+        )
+    }
+}
